485ff7b544e9c37fba288824e855957f9b2e679d
vanivilasam <65148965+vanivilasam@users.noreply.github.com>
2020-05-25 14:39:53 +0530

    c

---
+++ insert.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	<title>Php</title>
+		<meta http-equiv="Content-Type" content="text/html" charset="windows-1258">
+		<body>
+			<?php
+			$nm=$_GET["nm"];
+			$ct=$_GET["ct"];
+			$it=$_GET["it"];
+			
+			mysql_connect("vanivilasam.github.io","root","");
+			mysql_select_db("testing.sql")
+			
+			?>
+		</body>
+		</head>
+</html>
--- testing.sql
+++ testing.sql
@@ -1,39 +1,38 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 5.1.0-dev
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 28, 2017 at 02:20 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: 192.168.30.23
+-- Generation Time: May 25, 2020 at 12:08 PM
+-- Server version: 8.0.18
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `demo`
+-- Database: `testing`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `test`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `test` (
+  `Name` text NOT NULL,
+  `Phone NUmber` int(50) NOT NULL,
+  `Photot` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
